3bb8e2122fb122ff687e69d202a10d2dfd777e18
Christiana <christiana.john1@gmail.com>
2021-07-23 12:14:28 +0530

    Problem 21

---
+++ 21.py
@@ -0,0 +1,8 @@
+# FLOW010
+
+for i in range(int(input())):
+    classid=input()
+    if classid=="B" or classid=="b": print("BattleShip")
+    elif classid=="C" or classid=="c": print("Cruiser")
+    elif classid=="D" or classid=="d": print("Destroyer")
+    elif classid=="F" or classid=="f": print("Frigate")
