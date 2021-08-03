4d458ab7bc36bd3e144a6de2d91325d47fd06720
Christiana <christiana.john1@gmail.com>
2021-08-03 20:28:20 +0530

    code

---
+++ 38.py
@@ -0,0 +1,29 @@
+# TLG
+
+p1=p2=lead=0
+list5=[]
+for i in range(int(input())):
+    a,b = map(int,input().split())
+    p1=p1+a 
+    p2=p2+b 
+    lead=abs(p1-p2)
+    if(abs(p1))
+    list5.append(lead)
+    finlead= max(list5)
+    if(list3[x]>list4[x]):print("1" + " " + str(finlead))
+    else:print("2" + " " + str(finlead))
+    
+    # list1.append(a)
+    # list2.append(b)
+# print(list1)
+# print(list2)
+# for j in range(len(list1)):
+#     p1=p1+list1[j]
+#     p2=p2+list2[j]
+#     list3.append(p1)
+#     list4.append(p2)
+# print(list4)
+
+
+# for x in range(len(list1)):
+    
